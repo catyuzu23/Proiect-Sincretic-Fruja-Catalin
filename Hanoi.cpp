adc8e33a12c0665780c5848c4de5ec2382b98d03
catyuzu23 <cfruja@gmail.com>
2025-01-07 18:11:16 +0200

    Hanoi's function
    Implemented the logic behind Hanoi's algorithm in the required function

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -3,21 +3,47 @@
 #include <cmath>
 using namespace std;
 
-void towerOfHanoi(int nmbOfDisks){
-    stack<int> src, aux, dest;//our stacks neded 
-    int totalMoves;
-    for(int i=nmbOfDisks;i>=1;i--){
-        src.push(i);
-    }
+void moveDisk(){
+
 }
 
 
-int main(){
+void towerOfHanoi(int nmbOfDisks)
+{
+    stack<int> src, aux, dest; // our stacks needed
+    char s = 'A', a = 'B', d = 'C';
+    int totalMoves = pow(2, nmbOfDisks) - 1;
+    for (int i = nmbOfDisks; i >= 1; i--)
+    {
+        src.push(i);
+    }
+    if (nmbOfDisks % 2 == 0) // if we got an
+    {
+        swap(d, a);
+    }
+    for (int i = 0; i < totalMoves; i++)
+    {
+        if (i % 3 == 1)
+        {
+            moveDisk(src, dest, s, d);
+        }
+        else if (i % 3 == 2)
+        {
+            moveDisk(src, aux, s, a);
+        }
+        else if (i%3==0){
+            moveDisk(aux,dest,a,d);
+        }
+    }
+}
+
+int main()
+{
 
     int numberOfDisks;
-    
-    cout<<"How many disks you want? "<<endl;
-    cin>>numberOfDisks;
+
+    cout << "How many disks you want? " << endl;
+    cin >> numberOfDisks;
 
     towerOfHanoi(numberOfDisks);
     return 0;
